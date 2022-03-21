f6b47e71d99a1aefeea74e7e1a03d698c1e02d55
vladyslavpron <vladyslavpron@gmail.com>
2022-03-21 20:00:14 +0200

    login and register system

--- node-rest-api/routes/auth.js
+++ node-rest-api/routes/auth.js
@@ -5,19 +5,40 @@ const bcrypt = require("bcrypt");
 // Register
 router.post("/register", async (req, res) => {
   try {
+    // generate password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
+    // create new user
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
       password: hashedPassword,
     });
 
+    // save user and respond
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
-    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) res.status(404).send("user not found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) res.status(400).send("wrong password");
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 module.exports = router;
